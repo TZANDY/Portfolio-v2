0eed95c507efc1a05dbd1361e3f2313ce22c2c00
TZANDY <tzandy07@gmail.com>
2023-11-21 17:33:06 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Nav from './components/features/nav/Nav'
+
+function App() {
+
+
+  return (
+    <>
+    <Nav/>
+      <h1 className="text-3xl font-bold">
+        Hello world!
+      </h1>
+      
+    </>
+  )
+}
+
+export default App
