e533ccba528ba03ed17898e98f6925e2d0968408
Andi Infante <tzandy07@gmail.com>
2023-12-29 17:07:57 -0500

    conectando con el backend

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,11 +1,40 @@
 import { projects } from "../../helpers/data";
-import { Badge, Modal } from "flowbite-react";
-import { Card, Button } from "flowbite-react";
+import { Badge, Modal, Card, Button } from "flowbite-react";
 import "./projects.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ErrorPage from "../../utils/error-page";
 
 export default function Projects() {
   const [openModal, setOpenModal] = useState(false);
+  const [listaProyectos, setListaProyectos] = useState([]);
+
+  const llenarDatos = () => {
+    setListaProyectos([...projects])
+  }
+  const obtenerDatos = async () => {
+    try {
+      const res = await fetch("http://localhost:5190/api/Proyecto/Lista");
+      const data = await res.json();
+      setListaProyectos([...data.response]);
+    } catch (error) {
+      console.error("Error al obtener los datos: ", error)
+    }
+
+  }
+
+  useEffect(() => {
+    obtenerDatos();
+  }, []);
+
+  if (listaProyectos.length === 0) {
+    return (
+      <div>
+        <h1 className="text-white text-3xl">No hay proyectos disponibles</h1>
+      </div>
+    )
+  }
+
+
   return (
     <div className="flex flex-col items-center justify-center pt-20 pb-20 animate-fade-down animate-once animate-ease-out">
       <h3 className="text-slate-200 text-center items-center justify-center w-full text-4xl">
@@ -15,10 +44,10 @@ export default function Projects() {
         Integrando conocimientos y habilidades
       </span>
       <div className="grid m-2 gap-2 gap-y-3 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 pb-20 pt-10">
-        {projects.map((item) => {
+        {listaProyectos.map((p) => {
           return (
             <Card
-              key={item.id}
+              key={p.id}
               className="max-w-sm"
               renderImage={() => (
                 <img
@@ -31,23 +60,23 @@ export default function Projects() {
             >
               <div className="flex flex-row items-center">
                 <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {item.name}
-                  
+                  {p.nombre}
+
                 </h5>
-                <Badge color="warning" className="m-1">{item.version}</Badge>
+                <Badge color="warning" className="m-1">{p.version}</Badge>
               </div>
 
               <p className="font-normal text-gray-700 dark:text-gray-400">
-                {item.description}
+                {p.descripcion}
               </p>
               <div className="flex flex-wrap gap-2">
-                {item.tecnologias.map((tec, i) => (
+                {p.tecnologia.map((tec) => (
                   <Badge
                     color="success"
                     className="hover:animate-pulse hover:animate-infinite"
-                    key={i}
+                    key={tec.id}
                   >
-                    {tec}
+                    {tec.descripcion}
                   </Badge>
                 ))}
               </div>
@@ -60,7 +89,7 @@ export default function Projects() {
               </Button>
 
               <Modal show={openModal} onClose={() => setOpenModal(false)}>
-                <Modal.Header key={item.id}>Aviso</Modal.Header>
+                <Modal.Header>Aviso</Modal.Header>
                 <Modal.Body>
                   <div className="space-y-6">
                     <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
@@ -79,6 +108,7 @@ export default function Projects() {
           );
         })}
       </div>
+
     </div>
   );
 }
